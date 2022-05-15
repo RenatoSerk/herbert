57f7f155487937184eae7fd6e5a3618798440df6
RenatoSerk <brickworked@gmail.com>
2022-05-15 12:55:13 -0400

    Add splash screen

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../stylesheets/App.css';
+import { SplashScreen } from './SplashScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <SplashScreen/>
+      <header className="App-header">
+
+      </header>
+      <body>
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SplashScreen.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import '../stylesheets/SplashScreen.css';
+import shakespeare from '../resources/shakespeare.jpg';
+
+interface SplashScreenState{
+    fadeOutFinished: boolean;
+}
+
+// This should match up with the CSS value
+const SPLASH_SHOW_TIME_MS = 7000;
+
+export function SplashScreen(){
+
+    // Initial render only
+    const [mainState, setMainState] = React.useState<SplashScreenState>({fadeOutFinished: false});
+    React.useEffect(() => {
+        setTimeout(() => {
+            setMainState({...mainState, fadeOutFinished: true});
+        }, SPLASH_SHOW_TIME_MS);
+    });
+
+    // De-render HTML to match what the user sees
+    if(mainState.fadeOutFinished){
+        return null;
+    }
+    else{
+        return (
+            <header className={"SplashScreen"}>
+                <img src={shakespeare} alt={"Shakespeare"}/>
+                <p>&emsp; A million monkeys banging on a million typewriters will eventually reproduce the entire works of Shakespeare.</p>
+                <small className={"SplashScreen-subtext"}>- Unknown</small>
+            </header>
+        );
+    }
+}
